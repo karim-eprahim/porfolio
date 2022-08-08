b14b6852150db929fee95ec081c0f752ecf76c0b
karim-eprahim <102471709+karim-eprahim@users.noreply.github.com>
2022-08-08 15:28:52 +0200

    Update js.js
    scroll

--- js/js.js
+++ js/js.js
@@ -3,6 +3,8 @@ let nav =document.querySelector(".navbar")
 let round =document.querySelectorAll('.round a')
 let bage =document.querySelector(".bage")
 let menu =document.querySelectorAll(".menu-ptn span")
+//scroll element
+let scrol =document.querySelector(".scroll")
 
 console.log(menu)
 
@@ -14,4 +16,19 @@ for(let i=0; i<round.length;i++){
     round[i].onclick=()=>{
         nav.classList.toggle("open")
     }
+}
+//scroll method
+window.onscroll=()=>{
+    if(window.scrollY>=30){
+        scrol.style.cssText=`visibility:visible; z-index: 1;`
+    }else{
+        scrol.style.cssText=`visibility:hidden;`
+    }
+}
+scrol.onclick=()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+
 }
